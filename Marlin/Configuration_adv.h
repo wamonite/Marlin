1bf8f8ea3aabe9a47dba31969a67a965f26d37ea
Warren Moore <warren@wamonite.com>
2018-04-09 10:58:19 +0100

    Updated TMC2130 current defaults.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1037,13 +1037,16 @@
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
+  // WM: set default current
+  #define X_CURRENT          600  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
-  #define Y_CURRENT          800
+  // WM: set default current
+  #define Y_CURRENT          600
   #define Y_MICROSTEPS        16
 
-  #define Z_CURRENT          800
+  // WM: set default current
+  #define Z_CURRENT         1000
   #define Z_MICROSTEPS        16
 
   #define X2_CURRENT         800
@@ -1055,7 +1058,8 @@
   #define Z2_CURRENT         800
   #define Z2_MICROSTEPS       16
 
-  #define E0_CURRENT         800
+  // WM: set default current
+  #define E0_CURRENT         600
   #define E0_MICROSTEPS       16
 
   #define E1_CURRENT         800
